e88d54d799b84bd9667ff5fe6a2bf83ea36d9ef7
Volterra <volterra.luo@gmail.com>
2014-09-21 00:31:52 +0800

    rest_frame

--- jungle/settings.py
+++ jungle/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'appetizer',
     'rest_framework',
+    'restframe',
 )
 
 
@@ -61,8 +62,9 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGINATE_BY':10
 }
 
 ROOT_URLCONF = 'jungle.urls'
