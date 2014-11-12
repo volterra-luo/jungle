f0ad94b73a6a39776533e6bd035f09a1e25d8883
luocheng <cheng.luofd@gmail.com>
2014-11-12 22:55:52 +0800

    minor

--- appetizer/views.py
+++ appetizer/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-# @login_required
+@login_required
 def index(request):
    return render(request, 'quiz/index.html')
