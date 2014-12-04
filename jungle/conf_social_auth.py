1aa2673162889eba11dc19c094cca5a956507c7c
volterra-luo <volterra.luo@gmail.com>
2014-12-04 17:39:11 +0800

    virtualenv requirement.txt

--- jungle/conf_social_auth.py
+++ jungle/conf_social_auth.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-LOGIN_URL          = '/socialauth_douban/'
-LOGIN_REDIRECT_URL = '/quiz/'
-LOGIN_ERROR_URL    = '/login-error/'
+SOCIAL_AUTH_LOGIN_URL          = '/socialauth_douban/'
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/quiz/'
+SOCIAL_AUTH_LOGIN_ERROR_URL    = '/login-error/'
 
 SOCIAL_AUTH_COMPLETE_URL_NAME  = 'socialauth_complete'
 SOCIAL_AUTH_ASSOCIATE_URL_NAME = 'socialauth_associate_complete'
 
-DOUBAN2_CONSUMER_KEY = "0d43a2695456712d27cae73bfd035387"
-DOUBAN2_CONSUMER_SECRET = "79144fd570399fb2"
+
+
+SOCIAL_AUTH_DOUBAN_OAUTH2_KEY = "0d43a2695456712d27cae73bfd035387"
+SOCIAL_AUTH_DOUBAN_OAUTH2_SECRET = "79144fd570399fb2"
 
 AUTHENTICATION_BACKENDS = (
-    'social_auth.backends.contrib.douban.DoubanBackend2',                       
+    'social.backends.douban.DoubanOAuth2',               
     'django.contrib.auth.backends.ModelBackend',
 )
