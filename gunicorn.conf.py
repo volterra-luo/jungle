321ac213487440257892b3985d206c7e8670c00d
Volterra <volterra.luo@gmail.com>
2014-12-10 15:06:29 +0800

    modify gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,11 @@
 import multiprocessing
 
-bind = "127.0.0.1:2552"
+bind = '127.0.0.1:2552'
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = 'eventlet'
+worker_class = 'sync'
+accesslog = '/srv/jungle/log/gunicorn_access'
+errorlog = '/srv/jungle/log/gunicorn_error'
+loglevel = 'error'
+pythonpath = '/srv/jungle/www/jungle'
+user = 'volterra'
+group = 'volterra'
