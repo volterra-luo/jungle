4545e51ea7e459430c02691b2aea480c4343079c
volterra-luo <volterra.luo@gmail.com>
2015-01-16 23:45:05 +0800

    add myapp.js

--- jungle/static/jungle/js/myapp.js
+++ jungle/static/jungle/js/myapp.js
@@ -1,5 +1,36 @@
-$(document).ready(function() {
-      $( "#spinning-container" ).click( function() {
-            console.log( "You're on the logo!" );
-          });
-    });
+$( document ).ready( function() {
+	var node = $('#spinning-container').children(0);
+	var startDeg = random(360);
+	var timer;
+
+	node.css('background', 'none');
+	node.css({
+        top: random(40),
+        left: random(10),
+        zIndex: 1000
+      }).hover(
+          function() {
+            node.fadeTo(250, 1)
+                .css('zIndex', 1001)
+                .css('transform', 'rotate(0deg)');
+          },
+          function() {
+            node.fadeTo(250, .6).css('zIndex', 1000);
+            timer && clearTimeout(timer);
+            timer = setTimeout(spin, Math.ceil(random(1000)));
+            //setInterval(spin, Math.ceil(random(10000)));
+          }
+     	);
+
+    //setInterval(spin, Math.ceil(random(10000)));
+    
+    function spin() {
+        node.css('transform', 'rotate(' + random(360) + 'deg)');
+    }
+
+	// node.on('mouseover', function(event){
+	// 	console.log(random(10));
+	// });
+
+	function random(x) { return Math.random() * x };
+});
