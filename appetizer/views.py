dc13c182c170b7276cc912d9873d63a138caadff
Volterra <volterra.luo@gmail.com>
2015-02-20 17:57:15 +0800

    init account_login_view in appetizer views

--- appetizer/views.py
+++ appetizer/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from appetizer.forms import UserCreationForm
 
@@ -23,3 +24,12 @@ def account_create_view(request, **args):
 	username = None
 	local_args = { 'form': form, 'username': username, 'BASE_TMP': 'base/v0.1/base.html' }
 	return render(request, args.get('template'), local_args )
+
+def account_login_view(request, **args):
+	if request.method == 'POST':
+		pass
+	else:
+		form = UserCreationForm()
+
+	local_args = {'form': form, 'BASE_TMP': 'base/v0.1/base.html'}
+	return render(request, args.get('template'), local_args)
