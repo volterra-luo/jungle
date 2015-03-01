427e05bfbff807d32935ddb785ff57b85a44e4d6
Volterra <volterra.luo@gmail.com>
2015-03-01 21:22:47 +0800

    polish muster/forms.py

--- muster/forms.py
+++ muster/forms.py
@@ -79,77 +79,6 @@ class RegistrationForm(forms.Form):
 		help_text = u'这将是您的登录名',
 	)
 
-	# username = forms.EmailField(
-	# 	required = True,
-	# 	label = "E-mail",
-	# 	max_length = 100,
-	# 	help_text = "This will be your login name.",
-	# )
-
-	# username_again = forms.EmailField(
-	# 	required = True,
-	# 	label = "E-mail (again)",
-	# 	help_text = "Make sure that this is a valid E-mail address.",
-	# )
-
-	password = forms.CharField(
-		required = True,
-		label = u'密码',
-		widget = forms.PasswordInput(),
-		help_text = u'请使用大写字母，小写字母，数字等',
-	)
-
-	password_again = forms.CharField(
-		required = True,
-		label = u'密码确认',
-		widget = forms.PasswordInput(),
-	)
-
-	# first_name = forms.CharField(
-	# 	required = True,
-	# 	label = "First Name",
-	# 	help_text = "Enter your first name + middle name.",
-	# )
-
-	# def clean_first_name(self):
-	# 	first_name = self.cleaned_data['first_name']
-	# 	all_symbols = set(first_name)
-	# 	first_symbol = set(first_name[0])
-
-	# 	if ( first_symbol & _lower):
-	# 		raise forms.ValidationError('First letter must be Uppercase.')
-	# 	elif not ( all_symbols & _lower ):
-	# 		raise forms.ValidationError('Only CAPITALS are not allowed.')
-	# 	elif ( all_symbols & _digit ):
-	# 		raise forms.ValidationError('Digits are not allowed in first name.')
-
-	# 	return first_name
-
-	# last_name = forms.CharField(
-	# 	required = True,
-	# 	label = "Last Name",
-	# 	help_text = "Enter your last name.",
-	# )
-
-	# def clean_last_name(self):
-	# 	last_name = self.cleaned_data['last_name']
-	# 	all_symbols = set(last_name)
-	# 	first_symbol = set(last_name[0])
-
-	# 	if ( first_symbol & _lower):
-	# 		raise forms.ValidationError('First letter must be Uppercase.')
-	# 	elif not ( all_symbols & _lower ):
-	# 		raise forms.ValidationError('Only CAPITALS are not allowed.')
-	# 	elif ( all_symbols & _digit ):
-	# 		raise forms.ValidationError('Digits are not allowed in last name.')
-
-	# 	return last_name
-
-	captcha = CaptchaField(
-		required = True,
-		label = u'安全码',
-	)
-
 	def clean_username(self):
 		"""Make sure that `login` is unique in the system. """
 		username = self.cleaned_data['username']
@@ -161,17 +90,12 @@ class RegistrationForm(forms.Form):
 
 		raise forms.ValidationError('Username already in use.')
 
-	# def clean_username_again(self):
-	# 	"""Make sure that user verified `login` that he entered. """
-	# 	if 'username' in self.cleaned_data:
-	# 		username = self.cleaned_data['username']
-	# 		username_again = self.cleaned_data['username_again']
-
-	# 		if username == username_again:
-	# 			return username
-	# 		else:
-	# 			return None
-	# 		raise forms.ValidationError('E-mails do not match.')
+	password = forms.CharField(
+		required = True,
+		label = u'密码',
+		widget = forms.PasswordInput(),
+		help_text = u'请使用大写字母，小写字母，数字等',
+	)
 
 	def clean_password(self):
 		"""Make sure `password` isn't too easy to break. """
@@ -188,17 +112,31 @@ class RegistrationForm(forms.Form):
 
 		return password
 
+
+	password_again = forms.CharField(
+		required = True,
+		label = u'密码确认',
+		widget = forms.PasswordInput(),
+	)
+
 	def clean_password_again(self):
 		"""Make sure that the user verified the `password` he entered. """
 		if 'password' in self.cleaned_data:
 			password = self.cleaned_data['password']
 			password_again = self.cleaned_data['password_again']
 
-		if password == password_again:
-			return password
-		else:
-			return None
-		raise forms.ValidationError('Passwords do not match.')
+			if password == password_again:
+				return password
+			else:
+				raise forms.ValidationError('Passwords do not match.')
+
+
+	# captcha = CaptchaField(
+	# 	required = True,
+	# 	label = u'安全码',
+	# )
+
+
 
 class LoginForm(forms.Form):
 	"""Form for logging in users. """
@@ -230,6 +168,7 @@ class LoginForm(forms.Form):
 					raise forms.ValidationError('Your account was disabled.')
 					raise forms.ValidationError('Incorrect login name or password. Please try again.')
 
+
 class ReminderForm(forms.Form):
 	"""Password reminder form. """
 	username = forms.CharField(
@@ -251,6 +190,7 @@ class ReminderForm(forms.Form):
 			raise forms.ValidationError('Selected user does not exist.')
 
 		return username
+
 
 
 class UserProfileForm(forms.Form):
