8713c197b3f32036b05a300e76b6b3b1644c6198
Volterra <volterra.luo@gmail.com>
2015-03-03 16:33:24 +0800

    change create to register

--- muster/urls.py
+++ muster/urls.py
@@ -7,6 +7,7 @@ from muster import views
 
 urlpatterns = patterns('', 
 	url(r'^login/$', views.account_login_view, {'template': 'registration/login.html'}, name='login'),
-	url(r'^register/$', views.account_create_view, {'template':'registration/register.html'}, name='create', ),
-    	url(r'^', include('django.contrib.auth.urls')),
+	url(r'^register/$', views.account_register_view, {'template':'registration/register.html'}, name='register', ),
+	url(r'^logout/$', views.account_logout_view, name='logout' ),
+    	url(r'^$', include('django.contrib.auth.urls')),
 )
