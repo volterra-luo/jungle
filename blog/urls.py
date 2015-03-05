add11d472ea3eb50583ad91536c6bc6ab7f1acb1
Volterra <volterra.luo@gmail.com>
2015-03-05 20:57:15 +0800

    init blog app

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from blog import views
+
+urlpatterns = patterns('',	
+	url(r'^$', views.index),
+)
