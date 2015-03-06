430c4e9f5761a6118b18472521be5f0746a450df
Volterra <volterra.luo@gmail.com>
2015-03-06 22:50:20 +0800

    init models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+import uuid
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Article(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	datetime = models.DateTimeField(default=timezone.now())
+	author = models.ForeignKey(User)
