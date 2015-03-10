5313ab116c9319bb4a1a9323d41607ac45834793
volterra-luo <volterra.luo@gmail.com>
2015-03-10 15:54:50 +0800

    fix bug in Article model

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Article(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	# id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	title = models.CharField(max_length=100)
 	content = models.TextField()
-	pub_date = models.DateTimeField('date published', default=timezone.now())
+	pub_date = models.DateTimeField('date published', default=timezone.now)
 	author = models.ForeignKey(User)
