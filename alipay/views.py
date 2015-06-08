ce6ad4637c9bb8ca8217d9971223c4414248af39
volterra-luo <volterra.luo@gmail.com>
2015-06-08 21:32:42 +0800

    import requests

--- alipay/views.py
+++ alipay/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-# from util import AlipaySubmit
-# from config import AlipayConfig
+from util import AlipaySubmit
+from config import AlipayConfig
 
-import time, urllib, urllib2, logging, collections, mimetypes
+import time, urllib, urllib2, logging, collections, mimetypes, requests
 
 @login_required(login_url='/account/login/')
 def index(request):
 	return render(request, 'alipay/index.html')
+
+def alipay_submit(request):
+	if request.method == 'POST':
+		requests.post(url, data=payload)
+
 
 def return_view(request):
 	pass
