aa036683bbe99191f460ac76be1c2ea086169d90
volterra-luo <volterra.luo@gmail.com>
2015-06-08 21:27:30 +0800

    comment md5

--- alipay/sign/MD5.py
+++ alipay/sign/MD5.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 import hashlib
 
-
+#
+# 签名字符串
+# @param text 需要签名的字符串
+# @param key 密钥
+# @param input_charset 编码格式
+# @return 签名结果
+#
 def sign(text, key, input_charset):
 	m = hashlib.md5()
 	raw_str = text + key
@@ -10,6 +16,15 @@ def sign(text, key, input_charset):
 
 	return m.hexdigest()
 
+
+#
+# 验证签名字符串
+# @param text 需要签名的字符串
+# @param sign 签名结果
+# @param key 密钥
+# @param input_charset 编码格式
+# @return 验证签名结果
+#
 def verify(text, key, sign, input_charset):
 	
 	mysign = MD5.sign(text, key, input_charset)
