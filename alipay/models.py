63ac39a1313c5b8312d6527c6d5ba5c8b8eb9009
volterra-luo <volterra.luo@gmail.com>
2015-06-22 17:49:26 +0800

    add product model layout

--- alipay/models.py
+++ alipay/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	user_name = ''
+	out_trade_no = ''
+	subject = ''
+	total_fee = 0.01
+	created_date = ''
+	payment_date = ''
+	payment_status = ''
+
+	def __unicode__(self):
+		return self.user_name + '|' + self.out_trade_no
