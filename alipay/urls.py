36eeb1db61dfb34329c0edada60d25c39a3ddfb0
volterra-luo <volterra.luo@gmail.com>
2015-06-27 11:34:35 +0800

    add submit url

--- alipay/urls.py
+++ alipay/urls.py
@@ -4,6 +4,7 @@ from alipay import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<course_id>[0-9]+)/submit/$', views.alipay_submit, name='submit'),
     url(r'^receive_return/$', views.return_view, name='return_url'),
     url(r'^receive_notify/$', views.notify_view, name='notify_url'),
 )
