764346267358578999c4a282846ac1ffad9a48d5
volterra-luo <volterra.luo@gmail.com>
2015-07-02 16:42:11 +0800

    define product model

--- alipay/models.py
+++ alipay/models.py
@@ -1,16 +1,21 @@
+# -*- coding: utf-8 -*-
 import datetime
 
 from django.db import models
 from django.utils import timezone
 
 class Product(models.Model):
-	user_name = ''
-	out_trade_no = ''
-	subject = ''
-	total_fee = 0.01
-	created_date = ''
-	payment_date = ''
-	payment_status = ''
+	user_name = models.CharField(max_length=50)
+	out_trade_no = models.CharField(max_length=50)
+	trade_no = models.CharField(max_length=50)
+	subject = models.CharField(max_length=200)
+	total_fee = models.DecimalField(max_digits=6, decimal_places=2)
+	created_date = models.DateTimeField(auto_now_add=True)
+	payment_date = models.DateTimeField(auto_now=True)
+	payment_status = models.IntegerField()
+
+	class Meta:
+        ordering = ['payment_date', 'trade_no']
 
 	def __unicode__(self):
 		return self.user_name + ' | ' + self.out_trade_no
