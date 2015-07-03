87df2f072ea808ea07f80087067d78bce3d46e7a
volterra-luo <volterra.luo@gmail.com>
2015-07-03 13:53:57 +0800

    product model layout

--- alipay/models.py
+++ alipay/models.py
@@ -5,11 +5,33 @@ from django.db import models
 from django.utils import timezone
 
 class Product(models.Model):
-	user_name = models.CharField(max_length=50)
-	out_trade_no = models.CharField(max_length=50)
-	trade_no = models.CharField(max_length=50)
-	subject = models.CharField(max_length=200)
-	total_fee = models.DecimalField(max_digits=6, decimal_places=2)
+	
+	user_name = models.CharField(
+		'Username', 
+		max_length=20, 
+		unique=True, 
+	)
+
+	out_trade_no = models.CharField(
+		max_length=64, 
+		help_text='An unique numbered transaction ID in merchant system', 
+	)
+
+	trade_no = models.CharField(
+		max_length=28, 
+		help_text='The transaction ID in alipay system',
+	)
+
+	subject = models.CharField(
+		max_length=200,
+		help_text='A short description for the product.', 
+	)
+
+	total_fee = models.DecimalField(
+		max_digits=6, 
+		decimal_places=2
+	)
+
 	created_date = models.DateTimeField(auto_now_add=True)
 	payment_date = models.DateTimeField(auto_now=True)
 	payment_status = models.IntegerField()
