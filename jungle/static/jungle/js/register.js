2346128a4951d5ad8fe997d48062968ed0a3f67b
Volterra <volterra.luo@gmail.com>
2015-07-25 14:38:37 +0800

    add default username and password hint placeholder for registration.

--- jungle/static/jungle/js/register.js
+++ jungle/static/jungle/js/register.js
@@ -1,3 +1,15 @@
 $( document ).ready( function(){
 	$(':input').addClass("form-control");
+
+	$(function(){
+    		var username = $(':text');
+		var pwd = $(':password');
+    		username.hover(function(){
+      			$(this).attr('placeholder','test');
+    		});
+		pwd.hover(function(){
+			$(this).attr('placeholder','123456');
+		});
+   	});
+
 });
