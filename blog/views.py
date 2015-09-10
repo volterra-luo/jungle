2062a19b14286cec79a4625290c499538917f86a
volterra-luo <volterra.luo@gmail.com>
2015-09-10 11:47:14 +0800

    minor

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 from blog.models import Article
 
 def index(request):
-	blog = Article.objects.all()
-	return render(request, 'blog/detail.html', {'blog': blog[0]})
+	#blog = Article.objects.all()
+	#return render(request, 'blog/detail.html', {'blog': blog[0]})
+	return render(request,'blog/detail.html')
