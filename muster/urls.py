77ce104fdc5bba8bebe11f48db667f6dcccff437
volterra-luo <volterra.luo@gmail.com>
2015-09-19 22:45:05 +0800

    add password reset url in muster urls

--- muster/urls.py
+++ muster/urls.py
@@ -8,6 +8,8 @@ from muster import views
 urlpatterns = patterns('', 
 	url(r'^login/$', views.account_login_view, {'template': 'registration/login.html'}, name='login'),
 	url(r'^register/$', views.account_register_view, {'template':'registration/register.html'}, name='register', ),
+	url(r'^register-a/$', views.account_register_view_a, name='register-a', ),
 	url(r'^logout/$', views.account_logout_view, name='logout' ),
-    	url(r'^$', include('django.contrib.auth.urls')),
+	url(r'^password_reset/$', views.account_password_reset_view, name='password_reset' ),
+    url(r'^$', include('django.contrib.auth.urls'), ),
 )
