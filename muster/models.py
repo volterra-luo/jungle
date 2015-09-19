340ffcb0f96a34d1073cc5323223f531c73fb536
volterra-luo <volterra.luo@gmail.com>
2015-09-19 22:42:54 +0800

    modify muster models

--- muster/models.py
+++ muster/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User)
+    school = models.CharField(max_length=100)
+
+class Parent(models.Model):
+    user = models.OneToOneField(User)
+    kid = models.CharField(max_length=100)
