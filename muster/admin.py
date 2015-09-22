be7dc14e253d077a3d4bcd542d4ec78e791dae9b
volterra-luo <volterra.luo@gmail.com>
2015-09-22 23:05:06 +0800

    register Person Model in admin

--- muster/admin.py
+++ muster/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from muster.models import Person
 
-# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Person, PersonAdmin)
