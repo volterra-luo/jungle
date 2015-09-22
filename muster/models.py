64c2cd97befa75fe860cde45ca9d78b4af1916b1
volterra-luo <volterra.luo@gmail.com>
2015-09-22 23:02:25 +0800

    muster models

--- muster/models.py
+++ muster/models.py
@@ -8,5 +8,10 @@ class Person(models.Model):
 	is_admitted = models.BooleanField(default=False)
 	role = models.CharField(default='P', max_length=1)
 
+	def _mailbox(self):
+		return 'http://mail.' + self.user.email.split('@')[1]
+
+	mailbox = property(_mailbox)
+
 	def __unicode__(self):
 		return self.user.username
