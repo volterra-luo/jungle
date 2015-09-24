5fc5e581b721e87b51150251e9dc0b2b0b89cc9f
volterra-luo <volterra.luo@gmail.com>
2015-09-24 15:23:09 +0800

    adjust muster url.py

--- muster/urls.py
+++ muster/urls.py
@@ -11,6 +11,9 @@ urlpatterns = patterns('',
 	url(r'^register-a/$', views.account_register_view_a, name='register-a', ),
 	url(r'^logout/$', views.account_logout_view, name='logout' ),
 	url(r'^password_reset/$', views.account_password_reset_view, name='password_reset' ),
+
+	url(r'^email_verify/$', views.email_verify, name='email_verify'),
+
 	url(r'^thank/$', views.thank_view, name='thank'),
     url(r'^$', include('django.contrib.auth.urls'), ),
 )
