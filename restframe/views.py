cbf9ba2b6ffd6b4de2d81625604a54d336eaff65
volterra-luo <volterra.luo@gmail.com>
2015-09-30 21:39:49 +0800

    restframe views.py

--- restframe/views.py
+++ restframe/views.py
@@ -1,19 +1,25 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from restframe.serializers import UserSerializer, GroupSerializer
+from restframe.serializers import UserSerializer, PersonSerializer
+
+from muster.models import Person
+from rest_framework import generics
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class PersonViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
