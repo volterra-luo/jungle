6c428d6345d25e977e7d40a66600d7bd4fad31cd
volterra-luo <volterra.luo@gmail.com>
2015-09-30 21:33:12 +0800

    modify restframe urls.py

--- restframe/urls.py
+++ restframe/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import url, include
-from rest_framework import routers
+from rest_framework import routers, serializers, viewsets
 from restframe import views
 
-# Routers provide an easy way of automatically determining the URL conf.
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'persons', views.PersonViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+	url(r'^', include(router.urls)),
 ]
