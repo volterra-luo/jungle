50b65fe96633b72e83c292b41cf6c0626dacccb1
volterra-luo <volterra.luo@gmail.com>
2015-09-30 21:38:48 +0800

    add restframe serializers.py

--- restframe/serializers.py
+++ restframe/serializers.py
@@ -1,14 +1,20 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from muster.models import Person
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
+class UserSerializer(serializers.ModelSerializer):
+	person = serializers.PrimaryKeyRelatedField(read_only=True)
+
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'email', 'person')
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+
+	user = serializers.ReadOnlyField(source='user.username')
+
+	class Meta:
+		model = Person
+		fields = ('is_verified', 'is_admitted', 'user')
