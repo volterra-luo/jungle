9dbc6c9d6db1c3bad3183708f537ce470b2b971d
volterra-luo <volterra.luo@gmail.com>
2015-10-02 23:54:37 +0800

    add detail view in blog views.py

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from blog.models import Article
 @login_required(login_url='/account/login/')
 def index(request):
 	return render(request,'blog/index.html')
+
+@login_required(login_url='/account/login/')
+def detail(request):
+	return render(request,'blog/detail.html')
