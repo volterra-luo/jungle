dcd00ce90f856cb3a14f7627fdf12ed2e412da3f
volterra-luo <volterra.luo@gmail.com>
2015-10-04 23:10:20 +0800

    refine api view in restframe views.py

--- restframe/views.py
+++ restframe/views.py
@@ -1,10 +1,30 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from restframe.serializers import UserSerializer, PersonSerializer
 
 from muster.models import Person
 from rest_framework import generics
 
+@api_view(['GET', 'POST'])
+def nclab_user(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        p = Person.objects.get(user__id=pk)
+        serializer = PersonSerializer(p, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
