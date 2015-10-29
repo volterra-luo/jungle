699c7aaf4ed7cbebaac5ebf05ccfb824cd05d24e
volterra-luo <volterra.luo@gmail.com>
2015-10-29 23:03:33 +0800

    adjust blog views.py

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from blog.models import Article
 
-@login_required(login_url='/account/login/')
 def index(request):
 	return render(request,'blog/index.html')
 
 @login_required(login_url='/account/login/')
 def detail(request):
 	return render(request,'blog/detail.html')
+
+@login_required(login_url='/account/login/')
+def react_view(request):
+	return render(request,'blog/react.html')
