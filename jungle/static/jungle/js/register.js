26353417ba681893dcd0f6e854f8e092d5d64a86
volterra-luo <volterra.luo@gmail.com>
2015-11-05 00:00:35 +0800

    modify register.js

--- jungle/static/jungle/js/register.js
+++ jungle/static/jungle/js/register.js
@@ -1,20 +1,28 @@
 $( document ).ready( function(){
-	$(':input').addClass("form-control");
+	
+  $(':input').addClass("form-control");
 
 	$(function(){
+    var email = $('#id_email');
     var username = $(':text');
 		var pwd = $(':password');
-    		username.hover(function(){
-      			$(this).attr('placeholder','e.g. test');
-    		});
-		pwd.hover(function(){
-			$(this).attr('placeholder','e.g. 123456');
-		});
-   	});
 
-   	$('#termofuse').click(function(){
-   		if($(this).attr("checked")) {
-    		$('#submitB').attr("disabled",'disabled');
+    email.hover(function(){
+      $(this).attr('placeholder','somebody@awesome.com');
+    });
+
+    username.hover(function(){
+      $(this).attr('placeholder','e.g. somebody');
+    });
+
+    pwd.hover(function(){
+      $(this).attr('placeholder','e.g. top-secret');
+    });
+  });
+
+  $('#termofuse').click(function(){
+    if($(this).attr("checked")) {
+      $('#submitB').attr("disabled",'disabled');
     		$(this).attr("checked", false)
    		}
    		else {
